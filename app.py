7a8fffac3144fd67941cd2d07955777b5a9bfd24
mats-werrebrouck <mats-werrebrouck@outlook.be>
2024-02-16 15:33:58 +0100

    Clean code

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 
-# Generate a secure random key
 secret_key = secrets.token_hex(16)
 
 # Create a Flask app instance
 app = Flask(__name__)
 
-# Set the configuration for the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secret_key
@@ -17,12 +15,10 @@ app.config['SECRET_KEY'] = secret_key
 db = SQLAlchemy(app)
 app.app_context().push()
 
-# Create a model for the 'todo' table in the database
 class Todo(db.Model):
-    # Define the columns of the table
-    id = db.Column(db.Integer, primary_key=True) # Primary key for the table
-    task_name = db.Column(db.String(200), nullable=False) # Task name column (text with max length of 200 characters)
-    completed = db.Column(db.Boolean, default=False) # Completed column (boolean, default is False)
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
 
 # Create the 'todo' table in the database if it doesn't exist
 db.create_all()
@@ -33,26 +29,16 @@ def is_duplicate_task(task_name):
             return True
     return False
 
-# Define a route for the root URL with support for GET and POST methods
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # If the request method is POST (from submission), extract the task_name from the form data
         task_name = request.form['task_name']
+        if not is_duplicate_task(task_name) and task_name != '':
+            new_task = Todo(task_name=task_name, completed=False)
+            db.session.add(new_task)
+            db.session.commit()
 
-        # Create a new 'Todo' object with the task_name and default completed status as False
-        new_task = Todo(task_name=task_name, completed=False)
-
-        # Add the new_task object to the database session
-        db.session.add(new_task)
-        
-        # Commit the changes to the database, effectively inserting the new_task into the 'todo' table
-        db.session.commit()
-
-    # Retrieve all tasks from the 'todo' table
     tasks = Todo.query.all()
-
-    # Render the 'index.html' template and pass the tasks to it
     return render_template('index.html', tasks=tasks)
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
@@ -65,23 +51,16 @@ def edit_task(id):
             if not is_duplicate_task(new_task_name):
                 task_to_edit.task_name = new_task_name
                 db.session.commit()
-                return render_template('index.html', tasks=Todo.query.all())
+                return redirect('/')
         return render_template('edit.html', task=task_to_edit)
     else:
-        return render_template('index.html', tasks=Todo.query.all())
+        return redirect('/')
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_task(id):
-    # Get the task with the specified ID from the database
     task = Todo.query.get(id)
-
-    # Delete the task from the database
     db.session.delete(task)
-
-    # Commit the changes to the database
     db.session.commit()
-
-    # Redirect the user to the root URL
     return redirect('/')
 
 # Check if the script is being executed as the main program
